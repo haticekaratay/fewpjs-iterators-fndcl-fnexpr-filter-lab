e6b487878967af4a3b36f34a67c001520bc4f49e
Hatice <haticekaraty@gmail.com>
2021-07-25 23:08:16 -0400

    commit tree 1e97c97e6e6fbf75eedad16d6dae65573181af64

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(arrayDrivers,name) {
+   return arrayDrivers.filter(driverName => driverName.toLowerCase() === name.toLowerCase())
+}
+
+function fuzzyMatch(arrayDrivers,name){
+    return arrayDrivers.filter(driverName => driverName[0] == name[0])
+}
+
+function matchName(arrayDrivers,name){
+    return arrayDrivers.filter(driverObject => driverObject.name == name)
+}
